a5af75e7dc3b1b545209b84c8c4a0d08b5217cce
clem9281 <clem9281@gmail.com>
2019-03-26 14:33:46 -0700

    added endpoint for deleting zoo

--- routers/zooRoutes.js
+++ routers/zooRoutes.js
@@ -36,23 +36,40 @@ router
       );
   });
 
-router.route("/:id").get((req, res) => {
-  db("zoos")
-    .where({ id: req.params.id })
-    .first()
-    .then(zoo => {
-      if (!zoo) {
-        return res
-          .status(404)
-          .json({ error: "Sorry, we couldn't find a zoo at that id" });
-      }
-      res.status(200).json(zoo);
-    })
-    .catch(err =>
-      res
-        .status(500)
-        .json({ error: "We couldn't find the zoo you were looking for" })
-    );
-});
-
+router
+  .route("/:id")
+  .get((req, res) => {
+    db("zoos")
+      .where({ id: req.params.id })
+      .first()
+      .then(zoo => {
+        if (!zoo) {
+          return res
+            .status(404)
+            .json({ error: "Sorry, we couldn't find a zoo at that id" });
+        }
+        res.status(200).json(zoo);
+      })
+      .catch(err =>
+        res
+          .status(500)
+          .json({ error: "We couldn't find the zoo you were looking for" })
+      );
+  })
+  .delete((req, res) => {
+    db("zoos")
+      .where({ id: req.params.id })
+      .del()
+      .then(deleted => {
+        if (deleted === 0) {
+          return res
+            .status(404)
+            .json({ error: "There is no zoo entry at that id" });
+        }
+        db("zoos").then(zoos => res.status(200).json(zoos));
+      })
+      .catch(err =>
+        res.status(500).json({ error: "Sorry we couldn't delete that entry" })
+      );
+  });
 module.exports = router;
